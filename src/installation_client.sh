7dba90a637329c636d0fa7624311edfb5beb50c8
rbzt77 <102671289+rbzt77@users.noreply.github.com>
2022-03-30 15:43:44 +0200

    Create installation_client.sh

---
+++ src/installation_client.sh
@@ -0,0 +1,20 @@
+clear
+echo ""
+echo ""
+echo ""
+echo "			██████╗  █████╗ ██████╗ ██╗ ██████╗     ██████╗ ███████╗████████╗"
+echo "			██╔══██╗██╔══██╗██╔══██╗██║██╔═══██╗    ██╔══██╗╚══███╔╝╚══██╔══╝"
+echo "			██████╔╝███████║██║  ██║██║██║   ██║    ██████╔╝  ███╔╝    ██║   "
+echo "			██╔══██╗██╔══██║██║  ██║██║██║   ██║    ██╔══██╗ ███╔╝     ██║   "
+echo "			██║  ██║██║  ██║██████╔╝██║╚██████╔╝    ██████╔╝███████╗   ██║  " 
+echo "			╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝ ╚═════╝     ╚═════╝ ╚══════╝   ╚═╝ "
+echo "							       Installation"
+echo ""
+
+apt-get update
+apt-get install python3
+apt-get install python-pip
+apt-get install sshpass
+apt-get install python-tk
+echo ""
+echo "Fin de l'installation"

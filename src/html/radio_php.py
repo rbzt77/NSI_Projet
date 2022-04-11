ed9d0df5c898eb006ae1af4b46446e981d78ad0e
rbzt77 <102671289+rbzt77@users.noreply.github.com>
2022-04-11 22:18:15 +0200

    Create radio_php.py

---
+++ src/html/radio_php.py
@@ -0,0 +1,12 @@
+#lanceur radio car system() func en php bug avec le bash en brut 
+
+import os
+import sys
+from os import system as shell
+from sys import argv
+
+def main(musique=argv[1],frequence=argv[2]):
+  command = "sudo sox -t mp3 /home/pi/fm_transmitter/musiques/%s -t wav - | sudo /home/pi/fm_transmitter/fm_transmitter -f %s -r -"%(musique,frequence)
+  shell(command)
+if __name__=="__main__":
+  main()

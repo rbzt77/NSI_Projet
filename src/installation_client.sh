1882ededec37bc4eae21c684af6c26ac3ce56583
rbzt77 <102671289+rbzt77@users.noreply.github.com>
2022-04-19 23:06:56 +0200

    Update installation_client.sh

--- src/installation_client.sh
+++ src/installation_client.sh
@@ -16,5 +16,6 @@ sudo apt-get install python3
 sudo apt-get install python-pip
 sudo apt-get install sshpass
 sudo apt-get install python-tk
+#sudo echo "Ip du RPI3 radio-bzt" >> /etc/hosts 
 echo ""
 echo "Fin de l'installation"

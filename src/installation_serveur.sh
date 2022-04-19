6f8594628b9fd25dcaf2aad9dd478ecdaad9daee
RadioBZT <102671289+rbzt77@users.noreply.github.com>
2022-04-19 23:23:58 +0200

    Update installation_serveur.sh

--- src/installation_serveur.sh
+++ src/installation_serveur.sh
@@ -13,6 +13,7 @@ echo ""
 
 sudo apt-get update
 sudo apt-get install git
+sudo apt-get install sox libsox-fmt-mp3
 sudo git clone https://github.com/markondej/fm_transmitter
 cd fm_transmitter
 sudo apt-get install make gcc g++

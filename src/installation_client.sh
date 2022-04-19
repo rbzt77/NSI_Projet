5758b161371d1d723b3830d426665566f095c98d
rbzt77 <102671289+rbzt77@users.noreply.github.com>
2022-04-19 22:55:26 +0200

    Update installation_client.sh

--- src/installation_client.sh
+++ src/installation_client.sh
@@ -11,10 +11,10 @@ echo "			╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝
 echo "							       Installation"
 echo ""
 
-apt-get update
-apt-get install python3
-apt-get install python-pip
-apt-get install sshpass
-apt-get install python-tk
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install python-pip
+sudo apt-get install sshpass
+sudo apt-get install python-tk
 echo ""
 echo "Fin de l'installation"
